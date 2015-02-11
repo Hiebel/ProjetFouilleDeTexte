68005af989614d1047d4446a898b8be68b2c97da
DaemonWar <charbonnier.franck@gmail.com>
2015-02-11 23:27:14 +0100

    Jolis commentaires ! (2)

--- src/main/java/com/siad/blois/textmining/Main.java
+++ src/main/java/com/siad/blois/textmining/Main.java
@@ -12,12 +12,16 @@ public class Main
 	public static void main(String[] args)
 	{
 		Corpus corpus = Corpus.getInstance();
+		
+		System.out.println(">>Positifs");
 
 		Map<String, Integer> positifTokens = Utils.getTokens(corpus.getPositif(), "positif_token.txt");
 		
 		List<Word> positifWords = Utils.treeTagger(positifTokens);
 		
 		positifWords = Utils.setCategory(positifWords, '+');
+		
+		System.out.println(">>Négatifs");
 		
 		Map<String, Integer> negatifTokens = Utils.getTokens(corpus.getNegatif(), "negatif_token.txt");
 
